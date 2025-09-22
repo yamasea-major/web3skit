3192d856182827fec7b53a52655495f98c7fec70
liguixiang <liguixiang@1.com>
2025-09-22 13:05:33 +0900

    add home creator page

---
+++ skit_app/lib/ui/creator/sk_creator_home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SkCreatorHomePage extends StatefulWidget {
+  const SkCreatorHomePage({super.key});
+
+  @override
+  State<SkCreatorHomePage> createState() => _SkCreatorHomePageState();
+}
+
+class _SkCreatorHomePageState extends State<SkCreatorHomePage> {
+  List<int> top = <int>[];
+  List<int> bottom = <int>[0];
+
+  @override
+  Widget build(BuildContext context) {
+    const Key centerKey = ValueKey<String>('bottom-sliver-list');
+    return CustomScrollView(
+      center: centerKey,
+      slivers: <Widget>[
+        SliverList(
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Container(
+              alignment: Alignment.center,
+              // color: Colors.blue[200 + top[index] % 4 * 100],
+              // height: 100 + top[index] % 4 * 20.0,
+              child: Text('Item1'),
+            );
+          }, childCount: 40),
+        ),
+        SliverList(
+          key: centerKey,
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Container(
+              alignment: Alignment.center,
+              // color: Colors.blue[200 + bottom[index] % 4 * 100],
+              // height: 100 + bottom[index] % 4 * 20.0,
+              child: Text('Item2'),
+            );
+          }, childCount: 40),
+        ),
+      ],
+    );
+  }
+
+  //!end class
+}

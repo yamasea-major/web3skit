900e46c2995293179251179bd3d0f84b35b6eb42
liguixiang <liguixiang@1.com>
2025-09-22 14:45:11 +0900

    Fix

--- skit_app/lib/ui/setting/sk_setting_page.dart
+++ skit_app/lib/ui/setting/sk_setting_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:skit_app/ui/sk_ui_def.dart';
 
 class SkSettingPage extends StatefulWidget {
   const SkSettingPage({super.key});
@@ -8,14 +11,21 @@ class SkSettingPage extends StatefulWidget {
 }
 
 class _SkSettingPageState extends State<SkSettingPage> {
-
   //
 
   @override
   Widget build(BuildContext context) {
     //
     return Container(
-      color: Colors.amber
+      color: Colors.amber,
+      child: IconButton(
+          icon: Icon(
+            CupertinoIcons.back,
+            size: s32,
+          ),
+          onPressed: () {
+            GoRouter.of(context).pop();
+          }), // leadi
     );
   }
 
